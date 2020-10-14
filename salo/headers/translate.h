c4befef96e627ad59013a9002340baea164723f6
DDRDmakar <DDRDmakar@localhost.localdomain>
2020-10-14 15:00:13 +0300

    GPLv3

--- salo/headers/translate.h
+++ salo/headers/translate.h
@@ -1,15 +1,20 @@
 /*
 *
-*  Copyright ©, 2015-2020. All Rights Reserved.
-*
-*    Autors:
-*    Motylenok Mikhail
-*    Makarevich Nikita
-*
-*    This code is privately owned and is a commercial secret. We do not provide
-*    code to anyone without the written agreement. Copying, publication, use
-*    for commercial or non-commercial purposes without the consent
-*    of the authors is a violation of applicable law.
+* SaloIntellect project
+* Copyright (C) 2015-2020 Motylenok Mikhail, Makarevich Nikita
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 */
 #ifndef sequence_check_translate
